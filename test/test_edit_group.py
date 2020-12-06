59464b7f942f1fb26fbdc008eaaa544f70e90df7
Pav4l <pavel.gorev@gmail.com>
2020-12-06 19:07:42 +0300

    Добавлены проверки по списку групп в тесты на модификацию, добавление и удаление групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,4 +1,7 @@
 from model.group import Group
 
 def test_edit_group(app):
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="group_new", header="header_new", footer="footer_new"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
