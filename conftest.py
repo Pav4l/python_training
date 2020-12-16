5619232a3c676a41e792328b43bfdd225fe925f1
Pav4l <pavel.gorev@gmail.com>
2020-12-16 11:58:29 +0300

    Ещё один способ параметризации тестов: динамическая генерация тестов

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from fixture.application import Application
 import json
 import os.path
+import importlib
 
 fixture = None
 target = None
@@ -33,3 +34,12 @@ def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
     #parser.addoption("--username", action="store", default="target.json")
     #parser.addoption("--password", action="store", default="target.json")
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_form_module(module):
+    return importlib.import_module("data.%s" % module).testdata
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,11 +2,11 @@
 from model.contact import Contact
 import pytest
 import random
+from data.contacts import testdata
 
-from data.add_contact import testdata
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+#@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     if contact.group == "rand":
         contact.group = random.choice(app.group.get_group_list()).name
     old_contacts = app.contact.get_contact_list()
