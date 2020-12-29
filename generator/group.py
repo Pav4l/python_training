933277a2a315932fc9dd0418c7a02d3df4a9e142
Pav4l <pavel.gorev@gmail.com>
2020-12-29 22:54:54 +0300

    temp

--- generator/group.py
+++ generator/group.py
@@ -22,7 +22,7 @@ for o, a in opts:
         f = a
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + " "*10 #string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Group(name = "",header = "", footer = "")] + [
